b98e167a22bf6d2a3114bf61343a793d1a462466
Parth-gandhi-10 <parthgandhi246@gmail.com>
2025-09-08 11:33:51 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 class TestChurnModelTraining(unittest.TestCase):
     def test_model_training(self):
         # Load the saved churn model
-        model = joblib.load("model/churn_model.pkl")
+        model = joblib.load("churn_model.pkl")
 
         # Check that model is a RandomForestClassifier
         self.assertIsInstance(model, RandomForestClassifier)
